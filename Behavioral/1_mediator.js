703b2d7e8f69143d86a29ccea7d7ff7787e1dd6b
Mike Vagin <mvagin@bk.ru>
2021-05-27 07:36:54 +0300

    end Behavior/iterator

--- Behavioral/1_mediator.js
+++ Behavioral/1_mediator.js
@@ -9,3 +9,53 @@
 -
   Посредник может сильно раздуться.
 */
+
+class OfficialDealer {
+  constructor() {
+    this.customers = []
+  }
+  getCustomersList() {
+    return this.customers
+  }
+  addToCustomersList(name) {
+    this.customers.push(name)
+  }
+  orderAuto(customer, auto, info) {
+    const name = customer.getName()
+
+    console.log(`Order name: ${name}. Order auto is ${auto}`)
+    console.log(`Additional info: ${info}`)
+    this.addToCustomersList(name)
+  }
+}
+
+class Customer {
+  constructor(name, dealerMediator) {
+    this.name = name
+    this.dealerMediator = dealerMediator
+  }
+  getName() {
+    return this.name
+  }
+  makeOrder(auto, info) {
+    this.dealerMediator.orderAuto(this, auto, info)
+  }
+}
+
+// Example
+const mediator = new OfficialDealer()
+
+const user = new Customer('User', mediator)
+const user2 = new Customer('User2', mediator)
+
+user.makeOrder('Tesla', 'With autopilot!')
+/*
+Order name: User. Order auto is Tesla
+Additional info: With autopilot!
+*/
+user2.makeOrder('Audi', 'With parktronik!')
+/*
+Order name: User2. Order auto is Audi
+Additional info: With parktronik!
+*/
+console.log(mediator.getCustomersList()) // [ 'User', 'User2' ]
--- Behavioral/2_iterator.js
+++ Behavioral/2_iterator.js
@@ -9,3 +9,58 @@
 -
   Не оправдан, если можно обойтись простым циклом.
 */
+
+class Iterator {
+  constructor(elms) {
+    this.index = 0
+    this.keys = Object.keys(elms)
+    this.elements = elms
+  }
+  next() {
+    return this.elements[this.keys[this.index++]]
+  }
+  hasNext() {
+    return this.index < this.keys.length
+  }
+}
+
+// Example
+const collections = new Iterator(['Audi', 'Tesla', 'Bmw', 'Mersedes'])
+
+while (collections.hasNext()) {
+  console.log(collections.next())
+  /*
+    Audi
+    Tesla
+    Bmw
+    Mersedes
+   */
+}
+const autos = {
+  audi: {
+    model: 'Audi',
+    color: 'Black',
+    price: 20000
+  },
+  bmw: {
+    model: 'Bmw',
+    color: 'Red',
+    price: 30000
+  },
+  tesla: {
+    model: 'Tesla',
+    color: 'White',
+    price: 40000
+  }
+}
+
+const collections2 = new Iterator(autos)
+
+while(collections2.hasNext()) {
+  console.log(collections2.next())
+  /*
+    { model: 'Audi', color: 'Black', price: 20000 }
+    { model: 'Bmw', color: 'Red', price: 30000 }
+    { model: 'Tesla', color: 'White', price: 40000 }
+   */
+}
